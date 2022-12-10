2fb988d898629fd00a72847850e1c49e3de8f638
mattemonte03 <117922070+mattemonte03@users.noreply.github.com>
2022-12-10 18:47:22 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+#include "lib.h"
+float getMaxElement(float* array, float sizeOfArray) {
+    float resultingMaxValue = 0;
+    if (sizeOfArray > 0) {
+        resultingMaxValue = array[0];
 
+        for (int i = 0; i < sizeOfArray; ++i) {
+            if (array[i] > resultingMaxValue) {
+                resultingMaxValue = array[i];
+            }
+        }
+
+    }
+    return resultingMaxValue;
+}
