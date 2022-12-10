11708655e2ce4ac143fa9194cbf97ce8e044b571
mattemonte03 <117922070+mattemonte03@users.noreply.github.com>
2022-12-10 18:56:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include "lib.cpp"
 
 int main(){
+int arraySize=0;
+    std::cin>>arraySize;
+    float * array=new float[arraySize];
+    for(int i=0;i<arraySize;++i)
+std::cin>>array[i];
+    float maxValue=getMaxElement(array,arraySize);
+std::cout<<maxValue;  
   return 0;
 }
